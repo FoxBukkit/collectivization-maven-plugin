54bc58f82492d640298acfa9373f30aaa7a95be1
TomyLobo <tomylobo@nurfuerspam.de>
2013-12-08 02:23:48 +0100

    de-fail #2

--- src/main/java/eu/tomylobo/collectivization/Collectivizer.java
+++ src/main/java/eu/tomylobo/collectivization/Collectivizer.java
@@ -12,12 +12,10 @@ public class Collectivizer extends ClassVisitor {
     }
 
     public FieldVisitor visitField(int access, final String name, final String desc, final String signature, final Object value) {
-        if ((access & Opcodes.ACC_PUBLIC) != 0) {
-            return null;
+        if ((access & Opcodes.ACC_PUBLIC) == 0) {
+            access &= ~(Opcodes.ACC_PRIVATE | Opcodes.ACC_PROTECTED);
+            access |= Opcodes.ACC_PUBLIC;
         }
-
-        access &= ~(Opcodes.ACC_PRIVATE | Opcodes.ACC_PROTECTED);
-        access |= Opcodes.ACC_PUBLIC;
 
         return super.visitField(access, name, desc, signature, value);
     }
